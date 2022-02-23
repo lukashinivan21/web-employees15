9be3122c164c2769e5f5581f1809a047ca00e04d
Ivan <uvanes21@yandex.ru>
2022-02-23 07:28:28 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson15/webemployees15/WebEmployees15Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson15.webemployees15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployees15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployees15Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson15/webemployees15/WebEmployees15ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson15.webemployees15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployees15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
